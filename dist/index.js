305a6988f179292b9497c9086d965636cc570e35
omaresmaeel <omaresmael590@gmail.com>
2023-02-04 22:19:44 +0200

    test

--- dist/index.js
+++ dist/index.js
@@ -9782,39 +9782,35 @@ const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 async function run()
 {
     
-    const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
-    const octokit = new github.getOctokit(githubToken);
-    const { data: currentPulls } = await octokit.rest.pulls.list({
-        owner,
-        repo,
-        state: 'open'
-    });
-
-     currentPulls.forEach(async pull => {
-        pullNumber = pull.number
-        await updateBranch(octokit,pullNumber).then(() => {
-            console.log('quote');
-          }).catch((error) => {
-            console.error(error);
-          });
-        // if (respnse.status !== 200) {
-        //     console.log("pull request: "+pull.title+" can not be updated")
-        //     console.log("please visit "+pull.url+" to update it manually")
-        // } 
-        // else {
-        //     console.log("pull request: "+pull.title+" is updated")
-        // }  
-    })
-
-} 
-
-async function updateBranch(octokit,pullNumber) {
-    octokit.rest.pulls.updateBranch({
-        owner,
-        repo,
-        pull_number: pullNumber,
+        const githubToken = core.getInput("GITHUB_TOKEN", { required: true });
+        const octokit = new github.getOctokit(githubToken);
+        const { data: currentPulls } = await octokit.rest.pulls.list({
+            owner,
+            repo,
+            state: 'open'
         });
-}
+        try {
+            currentPulls.forEach(pull => {
+                pullNumber = pull.number
+                try {
+                console.log(octokit.rest.pulls.updateBranch({
+                    owner,
+                    repo,
+                    pull_number: pullNumber,
+                    }));
+                console.log("pull request: "+pull.title+" has been update")
+                }
+                catch (error) {
+                    console.log("pull request: "+pull.title+" can not be updated")
+                    console.log("please visit "+pull.url+" to update it manually")
+                }
+            })  
+        } catch (error) {
+            console.log(error)
+        }
+        
+
+    } 
     
     
 
